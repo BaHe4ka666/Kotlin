87f429151bdb86ebee469a0b2bc2fca6497495bf
BaHe4ka666 <soulerfun@gmail.com>
2024-11-30 12:31:16 +0500

    8.11 Приведение типов

--- src/corporation/Corp.kt
+++ src/corporation/Corp.kt
@@ -1,11 +1,13 @@
 import src.corporation.Assistant
 import src.corporation.Consultant
 import src.corporation.Director
+import src.corporation.Worker
 
 fun main() {
-    val director = Director("Vanya", 23)
-    val assistant = Assistant("Helen", 32)
-    val consultant = Consultant("Oleg")
-    director.getCoffee(assistant)
-    director.startWorkConsultant(consultant)
+    val director: Worker = Director("Vanya", 23)
+    val assistant: Worker = Assistant("Helen", 32)
+    val consultant: Worker = Consultant("Oleg") // UPCAST
+    (assistant as Assistant).bringCoffee() // DOWNCAST
+    (director as Director).startWorkConsultant(consultant as Consultant)
+    director.getCoffee(assistant) // SMARTCAST
 }
