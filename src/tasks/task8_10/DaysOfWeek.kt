47ebeca3601de2a776e5be02645b2d49b918f084
BaHe4ka666 <soulerfun@gmail.com>
2024-11-30 18:23:20 +0500

    8.14 TASK

---
+++ src/tasks/task8_10/DaysOfWeek.kt
@@ -0,0 +1,8 @@
+package src.tasks.task8_10
+
+enum class DaysOfWeek(
+    val title: String
+) {
+    SUNDAY("Воскресенье"), MONDAY("Понедельник"), TUESDAY("Вторник"), WEDNESDAY("Среда"),
+    THURSDAY("Четверг"), FRIDAY("Пятница"), SATURDAY("Суббота")
+}
--- src/tasks/task8_10/Task.kt
+++ src/tasks/task8_10/Task.kt
@@ -1,12 +1,16 @@
 package src.tasks.task8_10
 
 fun main() {
-    val price = Price(0, 0, 0)
-    val angleGrinder = AngleGrinder(0, true, true, " ", " ", 0, 0.0, 0, price)
-    val chainSaw = ChainSaw(0, 0, 0.0, " ", " ", 0, 0.0, 0, price)
-    val drill = Drill(0, 0, 0, " ", " ", 0, 0.0, 0, price)
-    val powerTools = listOf(angleGrinder, chainSaw, drill)
-    for (tool in powerTools) {
-        tool.turnOn()
+//    val price = Price(0, 0, 0)
+//    val angleGrinder = AngleGrinder(0, true, true, " ", " ", 0, 0.0, 0, price)
+//    val chainSaw = ChainSaw(0, 0, 0.0, " ", " ", 0, 0.0, 0, price)
+//    val drill = Drill(0, 0, 0, " ", " ", 0, 0.0, 0, price)
+//    val powerTools = listOf(angleGrinder, chainSaw, drill)
+//    for (tool in powerTools) {
+//        tool.turnOn()
+//    }
+    val daysOfWeek = DaysOfWeek.entries
+    for (day in daysOfWeek) {
+        println(day.title)
     }
 }
